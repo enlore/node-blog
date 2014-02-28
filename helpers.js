9e7043caa725a4730e32a71234cb9630a55b4045
enlore <n.e.lorenson@gmail.com>
2014-02-27 19:21:26 -0600

    Pull write fun out of config helper

--- helpers.js
+++ helpers.js
@@ -12,21 +12,27 @@ exports.getConfig = function (path) {
         }
     } catch (e) {
         if (e.code == 'ENOENT') {
-            var config = {
-                "app_host": "",
-                "app_port": "",
-                "app_secret": "",
-                "mongo_user": "",
-                "mongo_pwd": "",
-                "mongo_host": "",
-                "mongo_port": "",
-                "mongo_db_name": ""
-            }
-
-            fs.writeFileSync('config.json', JSON.stringify(config), {encoding: 'utf8'})
-
+            writeEmptyConfig()
             console.log('No config, yo, so I wrote a default one to the fs.  You should, you know, look at it.')
             process.exit(1)
+        } else {
+            throw e 
         }
     }
+}
+
+function writeEmptyConfig() {
+    var config = {
+        "app_host": "",
+        "app_port": "",
+        "app_secret": "",
+        "mongo_user": "",
+        "mongo_pwd": "",
+        "mongo_host": "",
+        "mongo_port": "",
+        "mongo_db_name": ""
+    }
+
+    fs.writeFileSync('config.json', JSON.stringify(config), {encoding: 'utf8'})
+
 }
